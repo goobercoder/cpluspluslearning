3e62bb14812add6316ffe748f71c45b62dc206e4
goobercoder <vaino.o.hanninen@gmail.com>
2024-12-11 21:16:12 +0200

    started programming on code rok

--- rok/main.cpp
+++ rok/main.cpp
@@ -1,12 +1,85 @@
 #include <iostream>
+//function declarations
+char getAndShowchoiseuser();
+char getAndShowchoisecomputeraiwoo();
+void determinewinner(char player, char computer);
+
 
 using std::cout;
 using std::cin;
 using std::endl;
 
 int main() {
+    char player;
+    char ai;
+    cout << "===================================" << endl;
+    cout << "rok papaer skissors gaem" << endl;
+    
 
+    player = getAndShowchoiseuser();
+    cout << "===================================" << endl;
+    ai = getAndShowchoisecomputeraiwoo();
+    cout << "===================================" << endl;
+    determinewinner(player, ai);
 
 
     return 0;
+}
+
+//function declarations
+char getAndShowchoiseuser() {
+    char player;
+    do{
+    cout << "===================================" << endl;
+    cout << "determine your choice" << endl;
+    cout << "r for rok" << endl;
+    cout << "s for skissor" << endl;
+    cout << "p for paeper" << endl;
+    cout << "now choose";
+    cin >> player;
+    }while(player != 'r' && player != 's' && player != 'p');
+
+    switch (player) {
+    case 'r':
+        cout << "you chose rok" << endl;
+        break;
+
+    case 'p':
+        cout << "you chose paeper" << endl;
+        break;
+
+    case 's':
+        cout << "you chose skissors" << endl;
+        break;
+    }
+
+    return player;
+}
+char getAndShowchoisecomputeraiwoo() {
+    srand(time(NULL));
+
+    int aiNum = (rand() % 3) + 1;
+    char aiChoise;
+
+    switch (aiNum)
+    {
+    case 1:
+        aiChoise = 'r';
+        cout << "ai chose rok";
+        break;
+    
+    case 2:
+        aiChoise = 's';
+        cout << "ai chose skissor";
+        break;
+
+    case 3:
+        aiChoise = 'p';
+        cout << "ai chose paeper";
+        break;
+    }
+    return aiChoise;
+}
+void determinewinner(char player, char computer) {
+    /*make the program choose winner.*/
 }
