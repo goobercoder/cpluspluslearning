cd2e7c1c74d3a53337a832cc94dd80489437f117
goobercoder <vaino.o.hanninen@gmail.com>
2024-12-12 23:37:45 +0200

    finalised learning of fill function (last commit from this pc)

--- fill/main.cpp
+++ fill/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::cin;
@@ -6,8 +7,22 @@ using std::endl;
 using std::string;
 
 int main() {
+    //the fill function
+    int size;
 
+    cout << "how big plz: ";
+    cin >> size;
 
+    string array[size];
+
+    fill(array, array + (size / 4), "goober");
+    fill(array + (size / 4), array + (size / 4)*2, "goog");
+    fill(array + (size / 4)*2, array + (size / 4)*3, "hehe");
+    fill(array + (size / 4)*3, array + size, "fucker");
+
+    for(string thing : array) {
+        cout << thing << ' ';
+    }
 
     return 0;
 }
