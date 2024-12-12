70d5803ac04c968e26e6391e5d42954c95dd8aca
goobercoder <vaino.o.hanninen@gmail.com>
2024-12-12 16:48:23 +0200

    learned about for each loops

--- foreach_loops/main.cpp
+++ foreach_loops/main.cpp
@@ -6,7 +6,15 @@ using std::endl;
 using std::string;
 
 int main() {
+    
+    //foreach loops can iterate over an iterable dataset
 
+    int dumb_numbers[] = {1, 3, 2, 5, 4, 10};
+
+    for(int dumb_number : dumb_numbers) {
+        cout << dumb_number << ' ';
+    }
+    
 
 
     return 0;
