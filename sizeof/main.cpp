fac3eb68b821eb93d3c89ac0e50e12b64e2c84f1
goobercoder <vaino.o.hanninen@gmail.com>
2024-12-12 15:08:36 +0200

    filled sizeof

--- sizeof/main.cpp
+++ sizeof/main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 
 using std::cout;
-using std::cin;
 using std::endl;
 using std::string;
 
 int main() {
+    /*
+    sizeof() operator returns the size of something in bytes.
+    */
 
+    string hello = "hello";
+    char things[] = {'h', 'y', 'j', 't'};
 
+    cout << sizeof(char) << " bytes" << endl;
+    cout << sizeof(hello) << " bytes" << endl;
+    cout << sizeof(things) << " bytes" << endl;
+
+    //with sizeof you can also see how big an array is
+    string names[] = {"goober", "doge", "cookie"};
+    cout << sizeof(names)/sizeof(string) << " elements\n";
 
     return 0;
 }
