4d3afaacdfa3d49d6c4ac04ebcd73930a6966db3
goobercoder <vaino.o.hanninen@gmail.com>
2024-12-14 17:00:52 +0200

    made some changes to multidimensional arrays

--- arrays/multidimensional/main.cpp
+++ arrays/multidimensional/main.cpp
@@ -11,18 +11,21 @@ int main() {
                                         {"mojang", "roblox", "saber interactive"},
                                         {"microsoft", "no parent company", "Beacon Interactive"}};
     
+    int rows = sizeof(gamesAndParentCompanies)/sizeof(gamesAndParentCompanies[0]);
+    int columns = sizeof(gamesAndParentCompanies[0])/sizeof(gamesAndParentCompanies[0][0]);
+
     cout << "games: ";
-    for(int i = 0; i < 3; i++) {
+    for(int i = 0; i < columns; i++) {
         cout << gamesAndParentCompanies[0][i] << ", ";
     }
 
     cout << endl << "companies: ";
-    for(int i = 0; i < 3; i++) {
+    for(int i = 0; i < columns; i++) {
         cout << gamesAndParentCompanies[1][i] << ", ";
     }
 
     cout << endl << "parent companies: ";
-    for(int i = 0; i < 3; i++) {
+    for(int i = 0; i < columns; i++) {
         cout << gamesAndParentCompanies[2][i] << ", ";
     }
 
