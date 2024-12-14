91051c9cf9f2acdadca8683230478190f90e4570
goobercoder <vaino.o.hanninen@gmail.com>
2024-12-14 12:40:41 +0200

    worked on userinputToArray and updated main.cpp template

--- userinputToArray/main.cpp
+++ userinputToArray/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::cin;
@@ -6,7 +7,26 @@ using std::endl;
 using std::string;
 
 int main() {
+    //putting user input into an array
+    string array[3];
+    int size = sizeof(array) / sizeof(array[0]);
+    string temp;
+    
 
+    for(int i = 0; i < size; i++) {
+        cout << "what is something you like ig or q to quit #" << i + 1 << ": ";
+        std::getline(std::cin, temp);
+        if(temp != "q") {
+            array[i] = temp;
+        }
+        else {
+            break;
+        }
+    }
+
+    for(string thing : array) {
+        cout << "you like: " << thing << endl;
+    }
 
 
     return 0;
