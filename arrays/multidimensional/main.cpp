59294331db11e50ee6de3ac060510f02184d2f77
goobercoder <vaino.o.hanninen@gmail.com>
2024-12-14 16:56:30 +0200

    multidimensional arrays

--- arrays/multidimensional/main.cpp
+++ arrays/multidimensional/main.cpp
@@ -7,8 +7,24 @@ using std::endl;
 using std::string;
 
 int main() {
+    string gamesAndParentCompanies[][3] = {{"minecraft", "ssrp", "space marine 2"},
+                                        {"mojang", "roblox", "saber interactive"},
+                                        {"microsoft", "no parent company", "Beacon Interactive"}};
+    
+    cout << "games: ";
+    for(int i = 0; i < 3; i++) {
+        cout << gamesAndParentCompanies[0][i] << ", ";
+    }
 
+    cout << endl << "companies: ";
+    for(int i = 0; i < 3; i++) {
+        cout << gamesAndParentCompanies[1][i] << ", ";
+    }
 
+    cout << endl << "parent companies: ";
+    for(int i = 0; i < 3; i++) {
+        cout << gamesAndParentCompanies[2][i] << ", ";
+    }
 
     return 0;
 }
