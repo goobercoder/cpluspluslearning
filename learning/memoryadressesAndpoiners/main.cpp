7b5b37b7a1448c34b43f166f1078dae4b2cb5e1c
goobercoder <vaino.o.hanninen@gmail.com>
2024-12-15 11:26:32 +0200

    did some memory adress stuff and added a folder for const parameters 	new file:   funcstuff/constParameters/compile.cmd 	new file:   funcstuff/constParameters/main.cpp 	modified:   memoryadressesAndpoiners/main.cpp 	modified:   memoryadressesAndpoiners/main.exe

--- learning/memoryadressesAndpoiners/main.cpp
+++ learning/memoryadressesAndpoiners/main.cpp
@@ -1,19 +1,53 @@
 #include <iostream>
 #include <string>
 
+
+
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
 
+void swap(string &x, string &y);
+void show(string x, string y);
+
 int main() {
-    string hello = "world";
+    string hello2 = "world";
     char grade = 'F';
     int gooberLevel = 1000000;
-    cout << &hello << ' ';
-    cout << &grade << ' ';    
-    cout << &gooberLevel << ' ';    
 
+    cout << &hello2 << ' ';
+    cout << &grade << ' ';    
+    cout << &gooberLevel << ' ' << endl;    
+
+
+
+    //you can use memory adresses to directly influence variables in functions
+    //memory adresses point to where the data of the variable is
+    //thus changing something where the adress is changes the data of the variable
+    
+    string hello = "hello";
+    string world = "world";
+
+    //this function makes copies of the variables and shows them
+    show(hello, world);
+
+    //this function uses memory adresses thus it can directly influence variables
+    swap(hello, world);
+
+    show(hello, world);
 
     return 0;
+}
+
+void show(string x, string y) {
+    cout << "hello: " << x << endl;
+    cout << "world: " << y << endl;
+}
+
+void swap(string &x, string &y) {
+    string temp;
+    temp = x;
+    x = y;
+    y = temp;
 }
