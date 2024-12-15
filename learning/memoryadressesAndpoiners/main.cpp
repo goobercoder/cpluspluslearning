f8f69d96153bf98854df07769d691f9576457f0d
goobercoder <vaino.o.hanninen@gmail.com>
2024-12-15 10:08:19 +0200

    did some memory adress stuff

--- learning/memoryadressesAndpoiners/main.cpp
+++ learning/memoryadressesAndpoiners/main.cpp
@@ -7,7 +7,12 @@ using std::endl;
 using std::string;
 
 int main() {
-
+    string hello = "world";
+    char grade = 'F';
+    int gooberLevel = 1000000;
+    cout << &hello << ' ';
+    cout << &grade << ' ';    
+    cout << &gooberLevel << ' ';    
 
 
     return 0;
