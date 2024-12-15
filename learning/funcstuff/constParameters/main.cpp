efee9c45f3f22ed13200917a56d3257a5c56ec2d
goobercoder <vaino.o.hanninen@gmail.com>
2024-12-15 13:51:44 +0200

    constparameters

--- learning/funcstuff/constParameters/main.cpp
+++ learning/funcstuff/constParameters/main.cpp
@@ -6,9 +6,31 @@ using std::cin;
 using std::endl;
 using std::string;
 
-int main() {
+void func1(string &thing);
+void func2(const string &thing);
 
+int main() {
+    /* const parameters help in protecting data that does not need to be changed
+    this is especially helpful when using memory adresses with functions*/
+
+    string thing = "do not change";
+    string thing2 = "do not change";
+
+    func1(thing);
+    func2(thing2);
 
 
     return 0;
+}
+
+void func1(string &thing) {
+    //thing was changed
+    thing = "changed";
+    cout << thing << endl;
+}
+
+void func2(const string &thing) {
+    //thing now cant be changed
+    //thing = "changed";
+    cout << thing << endl;
 }
