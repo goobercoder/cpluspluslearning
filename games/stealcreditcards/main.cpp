6dbca0db34f5fe12a079265d3d38e7a067f52b3b
goobercoder <vaino.o.hanninen@gmail.com>
2024-12-16 12:25:15 +0200

    started on credit card algorythim

---
+++ games/stealcreditcards/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int SumEvenNums(string Card);
+int SumOddNums(string Card);
+int GetDigit(int x);
+
+int main() {
+
+
+
+    return 0;
+}
+
+int SumEvenNums(string Card) {
+    
+}
+int SumOddNums(string Card);
+int GetDigit(int x);
+
