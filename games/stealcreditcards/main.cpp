8d9272d3b45185a21ebc539fd6145439b09d331e
goobercoder <vaino.o.hanninen@gmail.com>
2024-12-16 12:43:11 +0200

    finished credit card program

--- games/stealcreditcards/main.cpp
+++ games/stealcreditcards/main.cpp
@@ -11,15 +11,47 @@ int SumOddNums(string Card);
 int GetDigit(int x);
 
 int main() {
+    string Card;
+    int response = 0;
+    cout << "******************************" << endl;
+    cout << "credit card validator programm" << endl;
+    cout << "******************************" << endl;
+    cout << "input credit card number: " << endl;
+    cin >> Card;
 
+    response = SumEvenNums(Card) + SumOddNums(Card);
+
+    if(response % 10 == 0) {
+        cout << "credit card is valid";
+    }
+    else {
+        cout << "credit card is invalid";
+    }
+
+    
 
 
     return 0;
 }
 
 int SumEvenNums(string Card) {
+    int sum = 0;
+    for(int i = Card.size() -2; i >= 0; i -=2) {
+        sum += GetDigit((Card[i] - 48) * 2);
+    }
+    return sum;
+
     
 }
-int SumOddNums(string Card);
-int GetDigit(int x);
+int SumOddNums(string Card) {
+    int sum = 0;
+    for(int i = Card.size() - 1; i >= 0; i -=2) {
+        sum += Card[i] - 48;
+    }
+    return sum;
+}
+
+int GetDigit(int x) {
+    return x % 10 + (x / 10 % 10);
+}
 
